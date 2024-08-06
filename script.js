e440b995f7c775f94715d5786f97f3383b3fb63e
Georgio Mazraani <mazraanigeorgio@gmail.com>
2024-08-06 22:27:30 +0300

    update

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
-const video = document.getElementById('video');
+document.addEventListener('DOMContentLoaded', (event) => {
+    const video = document.getElementById('video');
 
-Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('https://georgiomazraani.github.io/face-detection/models/'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('https://georgiomazraani.github.io/face-detection/models/'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('https://georgiomazraani.github.io/face-detection/models/'),
-    faceapi.nets.faceExpressionNet.loadFromUri('https://georgiomazraani.github.io/face-detection/models/')
-]).then(startVideo)
+    Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri('https://georgiomazraani.github.io/face-detection/models/'),
+        faceapi.nets.faceLandmark68Net.loadFromUri('https://georgiomazraani.github.io/face-detection/models/'),
+        faceapi.nets.faceRecognitionNet.loadFromUri('https://georgiomazraani.github.io/face-detection/models/'),
+        faceapi.nets.faceExpressionNet.loadFromUri('https://georgiomazraani.github.io/face-detection/models/')
+    ]).then(startVideo);
 
-
-function startVideo() {
-    navigator.getUserMedia(
-        { video: {} },
-        stream => video.srcObject = stream,
-        err => console.error(err)
-    )
-}
-video.addEventListener('play', () => {
-    const canvas = faceapi.createCanvasFromMedia(video);
-    document.body.append(canvas);
-    const displaySize = {
-        width: video.width,
-        height: video.height
+    function startVideo() {
+        navigator.mediaDevices.getUserMedia({ video: {} })
+            .then(stream => {
+                video.srcObject = stream;
+                video.onloadedmetadata = () => {
+                    video.play();
+                    onPlay();
+                };
+            })
+            .catch(err => console.error("Error accessing camera: ", err));
     }
-    faceapi.matchDimensions(canvas, displaySize);
-    setInterval(async () => {
-        const detections = await faceapi.detectAllFaces(video,
-            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-        const resizedDetections = faceapi.resizeResults(detections, displaySize);
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-        faceapi.draw.drawDetections(canvas, resizedDetections);
-        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-    }, 100)
-})
+
+    function onPlay() {
+        const canvas = faceapi.createCanvasFromMedia(video);
+        document.body.append(canvas);
+        const displaySize = {
+            width: video.width,
+            height: video.height
+        };
+        faceapi.matchDimensions(canvas, displaySize);
+        setInterval(async () => {
+            const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+                .withFaceLandmarks()
+                .withFaceExpressions();
+            const resizedDetections = faceapi.resizeResults(detections, displaySize);
+            canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+            faceapi.draw.drawDetections(canvas, resizedDetections);
+            faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+            faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+        }, 100);
+    }
+});
